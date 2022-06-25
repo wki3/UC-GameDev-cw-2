67e9b79c0c570f131faf4a616608d3db2ec667a9
wki3 <walaa.jamaluddin@eng.ku.edu.kw>
2022-06-25 19:35:09 +0300

    Function exercise
    with the bonus

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿float playerSpeed = 30;
+
+compareSpeed(2.5f);
+
+void compareSpeed(float speed)
+{
+    Console.WriteLine("Old Player Speed is " + playerSpeed);
+    // playerspeed = speed 
+    Console.WriteLine("New Player Speed is " + speed);
+    if (playerSpeed > speed)
+    {
+        Console.WriteLine(playerSpeed + " is the Old speed (faster)");
+    }
+    else if (playerSpeed < speed)
+    {
+        Console.WriteLine(speed + " is the New speed (faster)");
+    }
+    else
+    {
+        Console.WriteLine(" the speed is the same");
+    }
+}
