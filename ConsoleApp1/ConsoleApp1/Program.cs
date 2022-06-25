ab674b125790f7efd72039afb668863f9f19b476
wki3 <walaa.jamaluddin@eng.ku.edu.kw>
2022-06-25 19:35:58 +0300

    If-else exercise

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+string heroName = "OREO OO";
+int heroPower = 303;
+
+string villainName = "Zahra aa";
+int villainPower = 812;
+
+if (heroPower < villainPower)
+{
+    Console.WriteLine("{0} is more powered than {1}", villainName , heroName);    
+}
+else if (heroPower > villainPower)
+{
+    Console.WriteLine("{1} is more powered than {0}", villainName, heroName);
+}
+else
+{
+    Console.WriteLine(" Both {0} and {1} have the same power", villainName, heroName);
+}
+
+
